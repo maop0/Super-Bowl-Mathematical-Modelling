a5f3b78b8640550a61d936cb4ca2b9dc2fe332d2
daniel2478178 <danielgcdphysics@gmail.com>
2025-11-19 00:28:09 +0800

    using latex

--- to_latex_table.py
+++ to_latex_table.py
@@ -12,7 +12,7 @@ def array_latex_table(mat):
     latex_str = ""
     for i in range(rows):
         row_str = " & ".join([str(mat[i, j]) for j in range(cols)])
-        latex_str += row_str + " \\\\\n"
+        latex_str += row_str + " \\"
     return latex_str
 
 def csv_latex_table(file_path):
@@ -24,7 +24,8 @@ def csv_latex_table(file_path):
     latex_str = df.to_latex(index=False, header=True)
     return latex_str
 
-array_latex_table(np.array(
-[[0.65217391, 0.66666667, 0.625     ],
- [0.2173913 , 0.22222222 ,0.25      ],
- [0.13043478 ,0.11111111, 0.125     ]]))
+array_latex_table(np.array([[0.05  ,     0.04225352 ,0.07416564 ,0.02912621 ,0.02325581],
+ [0.25   ,    0.21126761 ,0.17305315 ,0.34951456 ,0.34883721],
+ [0.35 ,      0.63380282 ,0.51915946 ,0.46601942 ,0.34883721],
+ [0.2   ,     0.07042254 ,0.12978986 ,0.11650485 ,0.20930233],
+ [0.15   ,    0.04225352 ,0.10383189 ,0.03883495 ,0.06976744]]).T)
